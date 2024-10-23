b02b3753a1fa9ee218e59daf9e57ed8de235a806
anthonybisgood <abisgood30@gmail.com>
2024-10-22 21:24:01 -0700

    Added basic logging on Main
    Settled on format for logging

--- src/main.py
+++ src/main.py
@@ -4,19 +4,26 @@ import sqlite3
 import subprocess
 import os
 import datetime
+import logging
 
 
 def __main__():
-    print(datetime.datetime.now())
-    initializeDB()
+    logging.basicConfig(
+        filename="../logs/log.log",
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(filename)s.%(funcName)s:%(lineno)d] %(message)s",
+    )
+    logger = logging.getLogger()
+    logger.debug("Starting main.py")
+    initializeDB(logger)
     dbConn = None
     cursor = None
     try:
         dbConn = sqlite3.connect("../data/budget.db")
-        print("Opened database successfully")
+        logger.debug("Opened database successfully")
         cursor = dbConn.cursor()
     except Exception as e:
-        print("Error opening database:", e)
+        logger.exception("Error opening database: %s", e)
         exit(0)
     bankInterface = BankInterface(cursor)
     clientIO = ClientIO(bankInterface)
@@ -27,26 +34,30 @@ def __main__():
     dbConn.close()
 
 
-def initializeDB():
+def initializeDB(logger):
     # if the db doesn't exist, run createDB.py
     if not os.path.exists("../data/budget.db"):
+        logger.debug("Initializing database")
         createDBFile = "createDB.py"
         result = subprocess.run(["python3", createDBFile], capture_output=True)
         if result.returncode != 0:
-            print(result.stderr.decode("utf-8"))
-            print("Error running createDB.py")
+            logger.exception(result.stderr.decode("utf-8"))
+            logger.error("Error running createDB.py")
             exit(0)
         else:
-            print("createDB.py ran successfully")
+            logger.debug("createDB.py ran successfully")
     # add transactions to the db
+    logger.debug("Adding transactions to the database")
     addToDBFile = "addToDB.js"
     result = subprocess.run(["node", addToDBFile], capture_output=True)
     if result.returncode != 0:
-        print("Error running addToDB.js")
+        logger.error(result.stderr.decode("utf-8"))
         exit(0)
     else:
-        print("addToDB.js ran successfully")
+        logger.debug("addToDB.js ran successfully")
+    logger.debug("Database initialized")
 
 
 if __name__ == __main__():
+
     __main__
